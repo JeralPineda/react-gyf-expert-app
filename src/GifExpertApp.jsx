7c81b94770784c8c6213ace2b1291d04427c16f4
Jeral Pineda <jeral.cris@gmail.com>
2023-01-05 20:52:39 -0600

    Agregando configuracion y estilos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,13 @@
 export const GifExpertApp = () => {
   return (
     <div>
+      {/* Titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gifs */}
+      {/* Gif Item */}
     </div>
   );
 };
