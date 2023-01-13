255aa6ed160bbf4dc5037620b4590bed4751abf1
Jeral Pineda <jeral.cris@gmail.com>
2023-01-12 21:45:11 -0600

    Evaluando la funcion del onNewCategory

--- src/tests/components/AddCategory.test.jsx
+++ src/tests/components/AddCategory.test.jsx
@@ -13,9 +13,11 @@ describe('Pruebas en <AddCategory />', () => {
 
   test('debe llamar onNewCategory si el input tiene un valor', () => {
     const inputValue = 'Bleach';
-    // TODO:
 
-    render(<AddCategory onNewCategory={() => {}} />);
+    //Mock (simulación)
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
 
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form');
@@ -23,6 +25,23 @@ describe('Pruebas en <AddCategory />', () => {
     fireEvent.input(input, { target: { value: inputValue } }); //Disparamos el evento
     fireEvent.submit(form);
 
-    expect(input.value).toBe('');
+    expect(input.value).toBe(''); //Que se limpio el formulario
+
+    expect(onNewCategory).toHaveBeenCalled(); //Que halla sido llamada
+    expect(onNewCategory).toHaveBeenCalledTimes(1); //Que halla sido llamada una vez
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue); //Que halla sido llamada con el valor de la caja de texto
+  });
+
+  test('No debe de llamar el onNewCategory si el input esta vació', () => {
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const form = screen.getByRole('form');
+
+    fireEvent.submit(form);
+
+    expect(onNewCategory).not.toHaveBeenCalled(); //Que no halla sido llamada
+    expect(onNewCategory).toHaveBeenCalledTimes(0); //Que halla sido llamada una 0 veces
   });
 });
